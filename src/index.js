b0ab35864ef1da47a68a1d6af4a5c20aa99bc104
Koen de Bont <koen.de.bont@maxserv.com>
2022-10-23 21:19:49 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-const { Client, Events, SlashCommandBuilder} = require('discord.js');
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+
+require('dotenv').config();
 
 const client = new Client({
-    intents: []
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
 });
 
+require('Event/Handler/onReadyEventHandler')(client, Events.ClientReady);
+require('Event/Handler/Message/onMessageCreateHandler')(client, Events.MessageCreate);
 
-client.on(Events.MessageCreate, event => {
 
+
+
+
+client.on(Events.ClientReady, () => {
+    console.log('im ready');
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+   console.log(interaction);
+});
+
+client.on(Events.MessageCreate, async message => {
+    if (message.author.bot) {
+        return;
+    }
+
+
+
+    // message.channel.send('hi there vsauce here');
 });
 
 client.login(process.env.TOKEN);
